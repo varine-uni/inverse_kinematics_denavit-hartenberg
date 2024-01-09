95d0a073ec718c7c44c51d393d1ac3f3d6028f8d
Ciaran May-Townsend <83427326+varine-uni@users.noreply.github.com>
2024-01-09 05:47:17 +0000

    Create DH transformation matrix function

---
+++ inverse_kinematics_denavit-hartenberg.py
@@ -0,0 +1,11 @@
+import numpy as np
+from mpl_toolkits import mplot3d
+
+# The Denavit-Hartenberg transformation matrix
+def dh_transform_matrix(theta, alpha, a, d):
+    return np.array([
+        [np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
+        [np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
+        [0, np.sin(alpha), np.cos(alpha), d],
+        [0, 0, 0, 1]
+    ])
