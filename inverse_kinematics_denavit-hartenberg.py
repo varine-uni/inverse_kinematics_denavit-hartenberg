c78780b6242a1a2d38816153e5a731aa99e2aeaf
Ciaran May-Townsend <83427326+varine-uni@users.noreply.github.com>
2024-01-09 06:05:43 +0000

    Update inverse_kinematics_denavit-hartenberg.py

--- inverse_kinematics_denavit-hartenberg.py
+++ inverse_kinematics_denavit-hartenberg.py
@@ -1,6 +1,8 @@
 import numpy as np
 from mpl_toolkits import mplot3d
 
+# Example using 3DOF
+
 # The Denavit-Hartenberg transformation matrix
 def dh_transform_matrix(theta, alpha, a, d):
     return np.array([
@@ -9,3 +11,14 @@ def dh_transform_matrix(theta, alpha, a, d):
         [0, np.sin(alpha), np.cos(alpha), d],
         [0, 0, 0, 1]
     ])
+    
+# Define DH parameters
+alpha = [0, 0, 0]
+a = [2, 0, 2]
+d = [0, 0, 0]
+theta = [np.pi*2, np.pi, np.pi]
+
+# Create matrices with parameters
+A1_0 = dh_transform_matrix(theta[0], alpha[0], a[0], d[0])
+A2_1 = dh_transform_matrix(theta[1], alpha[1], a[1], d[1])
+A3_2 = dh_transform_matrix(theta[2], alpha[2], a[2], d[2])
